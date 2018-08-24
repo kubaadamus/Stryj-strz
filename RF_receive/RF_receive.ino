94c589e4671b439917bb633c0f46e260b72e9bed
kubaadamus <jakubadamus1991@gmail.com>
2018-08-24 19:21:22 +0200

    ZMIENIAM PODEJŚCIE ze światła na nRF24L01

---
+++ RF_receive/RF_receive.ino
@@ -0,0 +1,53 @@
+#include <SPI.h>  
+#include "RF24.h" 
+long long oldTime=0;
+RF24 myRadio (7, 8); 
+struct package
+{
+  int id=0;
+
+};
+
+byte addresses[][6] = {"0"}; 
+
+
+
+typedef struct package Package;
+Package data;
+
+void setup() 
+{
+  Serial.begin(115200);
+  delay(1000);
+  myRadio.begin(); 
+  myRadio.setChannel(115); 
+  myRadio.setPALevel(RF24_PA_MAX);
+  myRadio.setDataRate( RF24_250KBPS ) ; 
+  myRadio.openReadingPipe(1, addresses[0]);
+  myRadio.startListening();
+
+  pinMode(5,OUTPUT);
+}
+
+
+void loop()  
+{
+  if ( myRadio.available()) 
+  {
+    while (myRadio.available())
+    {
+      myRadio.read( &data, sizeof(data) );
+    }
+    Serial.print(data.id);
+    if(data.id==1){
+      digitalWrite(5,HIGH);
+      oldTime=millis();
+      Serial.println("MOZNA");
+    }
+  }
+      if(millis()>oldTime+300){
+        digitalWrite(5,LOW);
+        Serial.println("NIE MOZNA");
+    }
+
+}
---
+++ RF_send/RF_send.ino
@@ -0,0 +1,39 @@
+#include <SPI.h>  
+#include "RF24.h"
+
+RF24 myRadio (7, 8);
+byte addresses[][6] = {"0"};
+
+struct package
+{
+  int id=1;
+};
+
+
+typedef struct package Package;
+Package data;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+  myRadio.begin();  
+  myRadio.setChannel(115); 
+  myRadio.setPALevel(RF24_PA_MAX);
+  myRadio.setDataRate( RF24_250KBPS ) ; 
+  myRadio.openWritingPipe( addresses[0]);
+  delay(1000);
+}
+
+void loop()
+{
+  myRadio.write(&data, sizeof(data)); 
+
+  Serial.print("\nPackage:");
+  Serial.print(data.id);
+  Serial.print("\n");
+  delay(100);
+
+
+}
